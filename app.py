08f7cf849804639462ecc66422196f60a31cd3b4
Nth Exception <84526496+akhil-rao@users.noreply.github.com>
2025-07-25 19:53:04 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from modules.customer_360 import run as run_customer_360
 
 st.set_page_config(page_title="Customer 360", layout="wide")
 
-st.sidebar.title("Modules")
+st.sidebar.title("Home")
 selected = st.sidebar.radio("Navigate", ["Customer 360 View"])
 
 if selected == "Customer 360 View":
