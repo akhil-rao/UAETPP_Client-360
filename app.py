b79fa3adc24c74370fee81381ea9dfe4e2d55a54
Nth Exception <84526496+akhil-rao@users.noreply.github.com>
2025-07-27 13:02:28 +0530

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-
 import streamlit as st
 from modules.customer_360 import run as run_customer_360
+from modules.customer_insights.customer_insights import run_customer_insights
 
 st.set_page_config(page_title="Customer 360", layout="wide")
 
-st.sidebar.title("Home")
-selected = st.sidebar.radio("Navigate", ["Customer 360 View"])
+st.sidebar.title("🏠 Navigation")
+selected = st.sidebar.radio("Navigate", [
+    "Customer 360 View",
+    "Customer Insights"
+])
 
 if selected == "Customer 360 View":
     run_customer_360()
+
+elif selected == "Customer Insights":
+    run_customer_insights()
